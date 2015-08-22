0962a491f66f4d0a7165abee86995bd3febb9ea2
Farhan Chauhan <farhan687@gmail.com>
2015-08-22 23:28:19 +0530

    Fix bulk insertion with 10 seconds of interval

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var url = 'http://stream.meetup.com/2/rsvps';
 var meetup = require('./include/meetup_fetch.js').meetup();
+
+//Start fetching
 meetup.fetch(url);
-//meetup.push_to_appbase();
+
+//push to appbase in 10 sec. interval
+setInterval(function(){
+	meetup.push_to_appbase();
+},10000);
 
--- include/meetup_fetch.js
+++ include/meetup_fetch.js
@@ -2,33 +2,34 @@ var http = require('http');
 var appbase = require("appbase-js")
 var elasticsearch = require('elasticsearch')
 var meetup_data = [];
-var type = {
+var fetch_response;
+var db = {
   index: 'createnewtestapp01',
   type: 'meetup',
-  //id: 1,
   body: {}
 };
+// app and authentication configurations  
+var HOSTNAME = "scalr.api.appbase.io"
+var APPNAME = "createnewtestapp01"
+var USERNAME = "RIvfxo1u1"
+var PASSWORD = "dee8ee52-8b75-4b5b-be4f-9df3c364f59f"
 
 exports.meetup = function() {
   return {
     fetch: fetch,
     push_to_appbase: push_to_appbase
   };
- 
+
   //Fetch  data here
   function fetch(url) {
     http.get(url, function(res) {
+      fetch_response = res;
       res.on('data', function(chunk) {
 
         //Parse json and store in meetup array
         try {
           var data = JSON.parse(chunk);
           meetup_data.push(data);
-
-          //Pause request push data to appbase
-          res.pause();
-          push_to_appbase(res, meetup_data);
-
         } catch (er) {
           //console.log('error: ' + er.message);
         }
@@ -41,57 +42,34 @@ exports.meetup = function() {
   }
 
   //Push to appbase
-  function push_to_appbase(fetch_response, meetup_data) {
-
-    // app and authentication configurations  
-    const HOSTNAME = "scalr.api.appbase.io"
-    const APPNAME = "createnewtestapp01"
-    const USERNAME = "RIvfxo1u1"
-    const PASSWORD = "dee8ee52-8b75-4b5b-be4f-9df3c364f59f"
-
-    var meetup_push_array = [];
-    for (var data_count = 0; data_count < meetup_data.length; data_count++) {
-      //type['id'] = data_count;
-      type['body'] = meetup_data[data_count];
-      meetup_push_array.push(type);
-    }
-
-    // console.log('body json is ready');
-    // console.log(meetup_push_array);
-
-    // elasticsearch client. we use it for indexing, mappings, search settings, etc.
+  function push_to_appbase() {   
     var client = new elasticsearch.Client({
       host: 'https://' + USERNAME + ":" + PASSWORD + "@" + HOSTNAME,
     });
 
-    
+    //Pause streaming
+    fetch_response.pause();
 
-    //For single record  
-    client.index(meetup_push_array[0]).then(function(response) {
-      console.log(response);
-      setTimeout(function() {
-        if(typeof fetch_response != 'undefined')
-          fetch_response.resume();
-      }, 5000);
-    }, function(error) {
-      console.log(error);
+    //Get all data and reformat it for elastic search.
+    var meetup_push_array = [];
+    for (var data_count = 0; data_count < meetup_data.length; data_count++) {
+      var type1 = meetup_data[data_count];
+      meetup_push_array.push({
+        index: {}
+      });
+      meetup_push_array.push(type1);
+    }
+
+    //For Bulk data
+    client.bulk({
+      index: db['index'],
+      type: db['type'],
+      body: meetup_push_array
+    }, function(err, resp) {
+      console.log("b_a: ", resp.items);
+      meetup_data = [];
+      fetch_response.resume();
     });
-
-//For Bulk data
-
-// client.bulk({
-//     index: 'createnewtestapp01',
-//     type: 'books',
-//     body: [{ index: { _id: '1'}},
-//       // document to be indexed
-//       jsonObject
-//     ]
-// }, function(err, resp) {
-//     console.log("b_a: ", resp);
-// });
-
-
-    // we will instantiate 'appbase' client when we need streams.
   }
 
 }
